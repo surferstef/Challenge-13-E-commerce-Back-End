311055a5830add41a700205c7228af1a326fa8b7
stefan hall <stefhall@comcast.net>
2021-06-20 21:32:40 -0700

    Revising Routes for finding single product & tag by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -38,7 +38,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findAll(
+  Product.findOne(
     {
       where: {
         id: req.params.id
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,52 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll(
+    {
+      attributes: [
+        "id", "tag_name"
+      ],
+      include: [
+      {
+        model: Product,
+        attributes: [
+          "id", "product_name", "price", "stock", "category_id"
+        ]
+      }
+    ]
+   })
+   .then(dbPost => res.json(dbPost))
+   .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        "id", "tag_name"
+      ],
+      include: [
+        {
+          model: Product,
+          attributes: [
+            "id", "product_name", "price", "stock", "category_id"
+          ]
+        }
+      ]
+    })
+    .then(dbTag => res.json(dbTag))
+   .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   });
 });
 
 router.post('/', (req, res) => {
