17fd52027d42e5dd30bb3e08900a05173d0f202f
stefan hall <stefhall@comcast.net>
2021-06-20 20:11:25 -0700

    Adding updates to Tag Model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define product id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    // define tag id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
