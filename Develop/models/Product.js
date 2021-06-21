96cdf9d0bc692914ef86398103083f4a03537aa8
stefan hall <stefhall@comcast.net>
2021-06-20 19:54:50 -0700

    Adding database models for category and product

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define product columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // define price column
+    price: {
+       type: DataTypes.DECIMAL,
+       allowNull: false,
+       validate: {
+         isDecimal: true
+       }
+    },
+   // define stock column
+   stock: {
+     id: DataTypes.INTEGER,
+     allowNull: false,
+     defaultValue: 10,
+     validate: {
+       isNumeric: true
+     }
+   },
+   // define category column
+   category_id: {
+     type: DataTypes.INTEGER,
+     references: {
+       model: "category",
+       key: "id"
+     }
+   }
   },
   {
     sequelize,
